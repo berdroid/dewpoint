1b8fc1e8b007d3545b8fde4d7f80f570281e925a
Bernhard <bernhard.bender@web.de>
2023-02-13 22:18:14 +0100

    added advanced linter & analyer rules

--- lib/dewpoint.dart
+++ lib/dewpoint.dart
@@ -1,46 +1,48 @@
 import 'dart:math';
 
-/// Calculate dew point in °C for a given
-/// temperature [t] in °C and rel humidity [h] in %
-///
-/// valid in temperature rage -40 ... 50°C
-///
-/// Source:
-/// Lawrence, M. (2005): The Relationship between Relative Humidity and the Dewpoint Temperature in Moist Air:
-/// A Simple Conversion and Applications. Bulletin of the American Meteorological Society,
-/// Vol. 86: Issue 2, p. 225
-///
-/// DOI:10.1175/BAMS-86-2-225
-/// https://journals.ametsoc.org/view/journals/bams/86/2/bams-86-2-225.xml
-///
-double calcDewPoint(double t, double h) {
-  const b = 17.269;
-  const c = 237.3;
+class DewPoint {
+  /// Calculate dew point in °C for a given
+  /// temperature [t] in °C and rel humidity [h] in %
+  ///
+  /// valid in temperature rage -40 ... 50°C
+  ///
+  /// Source:
+  /// Lawrence, M. (2005): The Relationship between Relative Humidity and the Dewpoint Temperature in Moist Air:
+  /// A Simple Conversion and Applications. Bulletin of the American Meteorological Society,
+  /// Vol. 86: Issue 2, p. 225
+  ///
+  /// DOI:10.1175/BAMS-86-2-225
+  /// https://journals.ametsoc.org/view/journals/bams/86/2/bams-86-2-225.xml
+  ///
+  static double calcDewPoint(double t, double h) {
+    const b = 17.269;
+    const c = 237.3;
 
-  final x = log(h / 100.0) + (b * t) / (c + t);
-  return (c * x) / (b - x);
-}
+    final x = log(h / 100.0) + (b * t) / (c + t);
+    return (c * x) / (b - x);
+  }
 
-/// Calculate absolute water in air in g/m³ for a given
-/// temperature [t] in °C and rel humidity [h] in %
-///
-/// valid in temperature rage -40 ... 50°C
-///
-/// Luftfeuchte
-/// https://www.wetterochs.de/wetter/feuchte.html
-double calcAbsoluteHumidity(double t, double h) {
-  final a = t > 0 ? 7.5 : 7.6;
-  final b = t > 0 ? 237.3 : 240.7;
+  /// Calculate absolute water in air in g/m³ for a given
+  /// temperature [t] in °C and rel humidity [h] in %
+  ///
+  /// valid in temperature rage -40 ... 50°C
+  ///
+  /// Luftfeuchte
+  /// https://www.wetterochs.de/wetter/feuchte.html
+  static double calcAbsoluteHumidity(double t, double h) {
+    final a = t > 0 ? 7.5 : 7.6;
+    final b = t > 0 ? 237.3 : 240.7;
 
-  // absolute temperature in K
-  final T = t + 273.25;
+    // absolute temperature in K
+    final T = t + 273.25;
 
-  // dew point
-  final td = calcDewPoint(t, h);
+    // dew point
+    final td = calcDewPoint(t, h);
 
-  // saturated water vapor pressure in hPa
-  final ps = 6.1078 * pow(10.0, (a * td) / (b + td));
+    // saturated water vapor pressure in hPa
+    final ps = 6.1078 * pow(10.0, (a * td) / (b + td));
 
-  // absolute water content in g/m³
-  return 218.42 * ps / T;
+    // absolute water content in g/m³
+    return 218.42 * ps / T;
+  }
 }
--- test/dewpoint_test.dart
+++ test/dewpoint_test.dart
@@ -1,51 +1,51 @@
 import 'package:dewpoint/dewpoint.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-main() {
+void main() {
   group('Dew point', () {
     test('near zero', () {
-      expect(calcDewPoint(20.0, 26.2), closeTo(0, 0.1));
-      expect(calcDewPoint(10.9, 47.0), closeTo(0, 0.1));
-      expect(calcDewPoint(0.0, 100.0), closeTo(0, 0.1));
+      expect(DewPoint.calcDewPoint(20.0, 26.2), closeTo(0, 0.1));
+      expect(DewPoint.calcDewPoint(10.9, 47.0), closeTo(0, 0.1));
+      expect(DewPoint.calcDewPoint(0.0, 100.0), closeTo(0, 0.1));
     });
 
     test('near 15', () {
-      expect(calcDewPoint(20.0, 73.0), closeTo(15, 0.1));
-      expect(calcDewPoint(37.0, 27.2), closeTo(15, 0.1));
-      expect(calcDewPoint(15.0, 100.0), closeTo(15, 0.1));
+      expect(DewPoint.calcDewPoint(20.0, 73.0), closeTo(15, 0.1));
+      expect(DewPoint.calcDewPoint(37.0, 27.2), closeTo(15, 0.1));
+      expect(DewPoint.calcDewPoint(15.0, 100.0), closeTo(15, 0.1));
     });
 
     test('near 30', () {
-      expect(calcDewPoint(55.0, 27.0), closeTo(30, 0.1));
-      expect(calcDewPoint(37.0, 67.6), closeTo(30, 0.1));
-      expect(calcDewPoint(30.0, 100.0), closeTo(30, 0.1));
+      expect(DewPoint.calcDewPoint(55.0, 27.0), closeTo(30, 0.1));
+      expect(DewPoint.calcDewPoint(37.0, 67.6), closeTo(30, 0.1));
+      expect(DewPoint.calcDewPoint(30.0, 100.0), closeTo(30, 0.1));
     });
   });
 
   group('Dew point', () {
     test('0°C', () {
-      expect(calcAbsoluteHumidity(0.0, 25.0), closeTo(1.2, 0.15));
-      expect(calcAbsoluteHumidity(0.0, 50.0), closeTo(2.4, 0.15));
-      expect(calcAbsoluteHumidity(0.0, 100.0), closeTo(4.8, 0.15));
+      expect(DewPoint.calcAbsoluteHumidity(0.0, 25.0), closeTo(1.2, 0.15));
+      expect(DewPoint.calcAbsoluteHumidity(0.0, 50.0), closeTo(2.4, 0.15));
+      expect(DewPoint.calcAbsoluteHumidity(0.0, 100.0), closeTo(4.8, 0.15));
     });
 
     test('20°C', () {
-      expect(calcAbsoluteHumidity(20.0, 25.0), closeTo(4.3, 0.15));
-      expect(calcAbsoluteHumidity(20.0, 50.0), closeTo(8.6, 0.15));
-      expect(calcAbsoluteHumidity(20.0, 100.0), closeTo(17.3, 0.15));
+      expect(DewPoint.calcAbsoluteHumidity(20.0, 25.0), closeTo(4.3, 0.15));
+      expect(DewPoint.calcAbsoluteHumidity(20.0, 50.0), closeTo(8.6, 0.15));
+      expect(DewPoint.calcAbsoluteHumidity(20.0, 100.0), closeTo(17.3, 0.15));
     });
 
     test('50%', () {
-      expect(calcAbsoluteHumidity(-10.0, 50.0), closeTo(1.2, 0.15));
-      expect(calcAbsoluteHumidity(5.0, 50.0), closeTo(3.4, 0.15));
-      expect(calcAbsoluteHumidity(22.0, 50.0), closeTo(9.7, 0.15));
+      expect(DewPoint.calcAbsoluteHumidity(-10.0, 50.0), closeTo(1.2, 0.15));
+      expect(DewPoint.calcAbsoluteHumidity(5.0, 50.0), closeTo(3.4, 0.15));
+      expect(DewPoint.calcAbsoluteHumidity(22.0, 50.0), closeTo(9.7, 0.15));
     });
   });
 
   group('Cold', () {
     test('-10°C', () {
-      expect(calcDewPoint(-10.0, 50.0), closeTo(-18.4, 0.15));
-      expect(calcAbsoluteHumidity(-10.0, 50.0), closeTo(1.2, 0.15));
+      expect(DewPoint.calcDewPoint(-10.0, 50.0), closeTo(-18.4, 0.15));
+      expect(DewPoint.calcAbsoluteHumidity(-10.0, 50.0), closeTo(1.2, 0.15));
     });
   });
 }
