72b2694df569055498209dcad232fa251832a5be
Bernhard <bernhard.bender@web.de>
2023-02-16 00:10:26 +0100

    removed bloc_test package - version conflict with flutter_test in sdk and dart_code_metrics

--- test/dewpoint_cubit_test.dart
+++ test/dewpoint_cubit_test.dart
@@ -1,23 +1,45 @@
-import 'package:bloc_test/bloc_test.dart';
+// ignore_for_file: avoid_print
+
+import 'dart:async';
+
 import 'package:dewpoint/cubit/dewpoint_cubit.dart';
 import 'package:flutter_test/flutter_test.dart';
 
 void main() {
-  group('Values', () {
-    blocTest<DewPointCubit, DewPointState>(
-      'emits update',
-      build: () => DewPointCubit(const DewPointState(temperature: 37.0)),
-      act: (cubit) => cubit.updateHumidity(27.2),
-      expect: () => [predicate((DewPointState state) => 14.9 < state.dewPoint && state.dewPoint < 15.1)],
-    );
+  late DewPointCubit cubit;
+
+  setUp(() {
+    cubit = DewPointCubit(const DewPointState());
+  });
+
+  tearDown(() {
+    return Future(() async {
+      await cubit.close();
+    });
   });
 
   group('Updates', () {
-    blocTest<DewPointCubit, DewPointState>(
-      'emits update with locked==true',
-      build: () => DewPointCubit(const DewPointState()),
-      act: (cubit) => cubit.updateLocked(locked: true),
-      expect: () => [predicate((DewPointState state) => state.locked)],
-    );
+    test('emits update', () {
+      unawaited(
+        expectLater(
+          cubit.stream,
+          emitsThrough(
+            predicate((DewPointState state) => 14.9 < state.dewPoint && state.dewPoint < 15.1),
+          ),
+        ),
+      );
+      cubit
+        ..updateTemperature(37.0)
+        ..updateHumidity(27.2);
+    });
+
+    test('emits update with locked==true', () {
+      unawaited(expectLater(cubit.stream, emitsThrough(predicate((DewPointState state) => state.locked))));
+      cubit.stream.listen(print);
+      cubit
+        ..updateTemperature(37.0)
+        ..updateHumidity(27.2)
+        ..updateLocked(locked: true);
+    });
   });
 }
