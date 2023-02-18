8118c0b81f565e3ec4383ae67fc1f18d97136cb8
Bernhard <bernhard.bender@web.de>
2023-02-18 13:15:09 +0100

    removed HookWidget, as features no longer used using StatelessWidget now

--- lib/pages/dewpoint_page.dart
+++ lib/pages/dewpoint_page.dart
@@ -4,9 +4,8 @@ import 'package:dewpoint/widgets/state_display.dart';
 import 'package:dewpoint/widgets/value_slider.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:flutter_hooks/flutter_hooks.dart';
 
-class DewPointPage extends HookWidget {
+class DewPointPage extends StatelessWidget {
   const DewPointPage({super.key, required this.title});
 
   final String title;
